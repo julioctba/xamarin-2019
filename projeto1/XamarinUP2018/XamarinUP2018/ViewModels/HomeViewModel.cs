85c386be89de67ce52a403e2c5eda5a3a45bee09
julioctba01 <julioctba01@gmail.com>
2019-07-20 11:30:16 -0300

    ultima versao

---
+++ projeto1/XamarinUP2018/XamarinUP2018/ViewModels/HomeViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace XamarinUP2018.ViewModels
+{
+    public sealed class HomeViewModel : ViewModelBase
+    {
+        private readonly IPageDialogService pageDialogService;
+
+        public HomeViewModel(INavigationService navigationService
+            , IPageDialogService pageDialogService): base (navigationService)
+        {
+            this.pageDialogService = pageDialogService;
+            ShowAlert = new DelegateCommand(async () => await ExecuteShowAllert())
+                .ObservesCanExecute(() => IsNotBusy);
+        }
+
+        private string text;
+        public string Text {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+
+        public ICommand ShowAlert { get; }
+
+        private async Task ExecuteShowAllert()
+        {
+            await ExecuteBusyAction(async() => {
+                await pageDialogService.DisplayAlertAsync("Hello", "The Message", "Ok");
+            });
+        }
+
+    }
+}
